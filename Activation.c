825dce25499b697364a7580f20484f3e97f0ed0b
Uliana <u.trubach08@gmail.com>
2026-09-19 17:14:09 +0300

    added Activation.c trubach

--- Activation.c
+++ Activation.c
@@ -1,37 +1,56 @@
-//
-// Created by Katya on 12.09.2026.
-//
-// task 2.23 (a)
 #include <stdio.h>
-#include <stdbool.h>
 #include <math.h>
+#include <stdbool.h>
 
-bool is_zero(double x) {
+
+double invsqrt(double x, double alpha){
+    if (alpha*x*x < -1) {
+        return NAN;
+    }
+    return x/sqrt(1+alpha*x*x);
+}
+
+double invsqrt_derivative(double x, double alpha) {
+    if (alpha*x*x < -1) {
+        return NAN;
+    }
+    double y = pow(1+alpha*x*x, 3);
+    return 1/sqrt(y);
+}
+
+bool iszero(double x) {
     return fabs(x) < 1e-9;
 }
 
-double onestep(double x) {
-    if (x >= 0) {
-        return 1;
-    } else {
-        return 0;
-    }
-}
-int test_onestep() {
-    if (!is_zero(onestep(-1))) {
-        printf("Test onestep for -1 is failed\n");
+int tests() {
+    printf("tests ...\n");
+    if (!iszero(invsqrt(0.0, 0.0))) {
+        printf("invsqrt(0.0, 0.0) != 0\n");
         return 1;
     }
-    if (!is_zero(onestep(0)-1)) {
-        printf("Test onestep for 0 is failed\n");
+    if (!iszero(invsqrt(1.0, 3.0)-0.5)) {
+        printf("invsqrt(1.0, 3.0) != 0.5\n");
         return 1;
     }
-    if (!is_zero(onestep(2)-1)) {
-        printf("Test onestep for 2 is failed\n");
+    if (!isnan(invsqrt(-1.0, -5.0))) {
+        printf("invsqrt(-1.0, -5.0) != nan\n");
         return 1;
     }
+    if (!iszero(invsqrt_derivative(0.0, 0.0)-1.0)) {
+        printf("invsqrt_derivative(0.0, 0.0) != 1\n");
+    }
+    if (!isnan(invsqrt_derivative(1.0, -3.0))) {}
+    printf("tests are successful\n");
     return 0;
 }
-double onestep_derivative(double x) {
-    return 0;
+
+int main() {
+    double x, alpha;
+    tests();
+    printf("input x and alpha: ");
+    scanf("%lf %lf", &x, &alpha);
+    double i = invsqrt(x, alpha);
+    double j = invsqrt_derivative(x, alpha);
+    printf("invsqrt(%lf, %lf) = %lf\n", x, alpha, i);
+    printf("invsqrt_derivative(%lf, %lf) = %lf\n", x, alpha, j);
 }
